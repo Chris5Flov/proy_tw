93a0c34905fc77bdeef2a79f6d524d316602afe8
Chris5Flov <c.tend145@gmail.com>
2025-12-03 21:46:33 -0600

    funcionamiento descargas arch temp

--- public/descargas.php
+++ public/descargas.php
@@ -1,9 +1,22 @@
+<?php
+$mysqli = require __DIR__ . "/login/database.php"; 
+
+require_once __DIR__ . "/../p12/ArchivosManager.php"; 
+
+$filtro_activo = $_GET['tipo'] ?? 'Todos'; 
+
+$manager = new ArchivosManager($mysqli); 
+
+$archivos_disponibles = $manager->obtenerArchivos($filtro_activo);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>Descargas</title>
+    <title>Downloads</title>
     <link rel="stylesheet" href="styles.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
@@ -58,7 +71,7 @@
                     $icon_class = match (strtoupper($archivo['tipo'])) {
                         'XML' => 'fa-file-code',
                         'PDF' => 'fa-file-pdf',
-                        'EXE' => 'fa-file-powerpoint',
+                        'EXE' => 'fa-file-powerpoint', 
                         'JSON' => 'fa-file-code', 
                         default => 'fa-file',
                     };
